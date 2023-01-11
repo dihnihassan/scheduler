88ee0049835f144e742661cb6ef22c6e5cbf8b78
Dihni Hassan <dihnihassan@gmail.com>
2023-01-11 15:05:11 -0500

    Build the Interviewer Item

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+
+// {id: number, name: string, avatar: url, selected: boolean}
+function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
+  return (
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
+  <img
+    className="interviewers__item-image"
+    src="https://i.imgur.com/LpaY82x.png"
+    alt="Sylvia Palmer"
+  />
+  {/* {props.name or props.children} */}
+  {props.name}
+</li>
+  )
+}
+
+export default InterviewerListItem;
