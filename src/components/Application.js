bece74010637ea5595d7eba777d73ccf550a69ff
Dihni Hassan <dihnihassan@gmail.com>
2023-01-12 20:17:45 -0500

    Finish all requirements from Retrieving Appointments activity in Compass

--- src/components/Application.js
+++ src/components/Application.js
@@ -9,6 +9,9 @@ import DayList from "./DayList";
 import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment";
 
+import { getAppointmentsForDay } from "helpers/selectors";
+import { getByDisplayValue } from "@testing-library/react";
+
 // const days = [
 //   {
 //     id: 1,
@@ -27,56 +30,84 @@ import Appointment from "components/Appointment";
 //   },
 // ];
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  const setDay = day => setState({ ...state, day });
+  // const setDays = (days) => {
+  //   setState(prev => ({ ...prev, days }));
+  // }
 
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  let dailyAppointments = [];
+
+  dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  console.log("Daily Appointments", dailyAppointments);
+  
 useEffect(() => {
-  axios.get('/api/days').then((response) => {
-    setDays([...response.data]);
-    console.log(response.data.days);
+  Promise.all([
+  axios.get('/api/days'),
+  axios.get('api/appointments'),
+  axios.get('api/interviewers')
+  ]).then((all) => {
+    setState(state => ({...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
+    // setDays([...response.data]);
+    console.log(all[1].data);
   })
 }, []);
+
+
+// useEffect(() => {
+//   axios.get('/api/days').then((response) => 
+//   console.log("Response", response));
+// }, []);
+
 
   return (
     <main className="layout">
@@ -89,8 +120,8 @@ useEffect(() => {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
@@ -101,7 +132,7 @@ useEffect(() => {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appointment) => {
+        {dailyAppointments.map((appointment) => {
           return (
             <Appointment 
             key={appointment.id} 
